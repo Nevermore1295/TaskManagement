c8af228db914632e520001dd6be9e557ca09c450
Nevermore1295 <hoangductrunggsa9999@gmail.com>
2025-02-13 08:15:43 +0700

    sql2 update
    - API route
    - SQL connection tested
    - restructured

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Welcome to My Next.js App with App Router</h1>
+      <p>API Routes are still located in the `pages/api` directory.</p>
+      <p>Visit <a href="/api/test">/api/test</a> to test the API.</p>
+    </div>
   );
 }
+
