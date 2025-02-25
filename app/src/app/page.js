7235ccc4ef8debffe762ec85c5425de41f15eae7
Nevermore1295 <hoangductrunggsa9999@gmail.com>
2025-02-25 16:36:52 +0700

    package merge

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div>
       <h1 className="flex w-100">Welcome to My Next.js App with App Router</h1>
-      <p><a href="LoginPage">LoginPage</a></p>
-      <p><a href="RegisterPage">RegisterPage</a></p>
+      <p><a href="Login">LoginPage</a></p>
+      <p><a href="Register">RegisterPage</a></p>
     </div>
   );
 }
